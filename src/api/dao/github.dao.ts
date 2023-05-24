1ebaac2c7a837f3ccdd424da3f5f9087da181798
BJTowne <bjtowne9@gmail.com>
2023-05-24 19:40:24 -0400

    Fixed Github ENV

--- src/api/dao/github.dao.ts
+++ src/api/dao/github.dao.ts
@@ -6,11 +6,77 @@ export type GithubIssue = {
   body: string;
 };
 
+export interface GithubIssueResponse {
+  url:                      string;
+  repository_url:           string;
+  labels_url:               string;
+  comments_url:             string;
+  events_url:               string;
+  html_url:                 string;
+  id:                       number;
+  node_id:                  string;
+  number:                   number;
+  title:                    string;
+  user:                     User;
+  labels:                   any[];
+  state:                    string;
+  locked:                   boolean;
+  assignee:                 null;
+  assignees:                any[];
+  milestone:                null;
+  comments:                 number;
+  created_at:               Date;
+  updated_at:               Date;
+  closed_at:                null;
+  author_association:       string;
+  active_lock_reason:       null;
+  body:                     string;
+  closed_by:                null;
+  reactions:                Reactions;
+  timeline_url:             string;
+  performed_via_github_app: null;
+  state_reason:             null;
+}
+
+export interface Reactions {
+  url:         string;
+  total_count: number;
+  "+1":        number;
+  "-1":        number;
+  laugh:       number;
+  hooray:      number;
+  confused:    number;
+  heart:       number;
+  rocket:      number;
+  eyes:        number;
+}
+
+export interface User {
+  login:               string;
+  id:                  number;
+  node_id:             string;
+  avatar_url:          string;
+  gravatar_id:         string;
+  url:                 string;
+  html_url:            string;
+  followers_url:       string;
+  following_url:       string;
+  gists_url:           string;
+  starred_url:         string;
+  subscriptions_url:   string;
+  organizations_url:   string;
+  repos_url:           string;
+  events_url:          string;
+  received_events_url: string;
+  type:                string;
+  site_admin:          boolean;
+}
+
 export async function createIssue(
   repo: string,
   issue: GithubIssue
-): Promise<void> {
-  await request({
+): Promise<GithubIssueResponse> {
+  return (await request({
     name: "Create Github Issue",
     url: `${AppConf.githubApiBaseUrl}/repos/${repo}/issues`,
     method: "POST",
@@ -21,5 +87,5 @@ export async function createIssue(
       Accept: "application/vnd.github+json",
     },
     body: issue,
-  });
+  })).json();
 }
